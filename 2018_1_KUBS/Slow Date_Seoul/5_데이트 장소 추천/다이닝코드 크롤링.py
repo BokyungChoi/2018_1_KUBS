46bfa1ce32637fd8431d6c45aa19ce379fb8ae23
Bokyung Choi <39907231+BokyungChoi@users.noreply.github.com>
2019-03-23 01:52:36 +0900

    Update 다이닝코드 크롤링.py

--- 2018_1_KUBS/Slow Date_Seoul/5_데이트 장소 추천/다이닝코드 크롤링.py
+++ 2018_1_KUBS/Slow Date_Seoul/5_데이트 장소 추천/다이닝코드 크롤링.py
@@ -9,6 +9,10 @@ Dining Code 키워드별 1위-10위 맛집 크롤링
 
 '''
 
+import os
+import pandas as pd
+import numpy as np
+
 #함수 코딩
 import requests as req
 from bs4 import BeautifulSoup
@@ -30,7 +34,7 @@ def dining_code(q):
 
 
 #함수 활용
-dining_code('한양대')
+dining_code('역 이름')
 
 
 # In[47]:
@@ -40,33 +44,6 @@ dining_code('한양대')
 dining_rank=pd.DataFrame()
 for i in station_list:
     dining_rank=dining_rank.append(pd.Series(dining_code(i)),ignore_index=True)
-
-
-# In[5]:
-
-
-import os
-import pandas as pd
-import numpy as np
-import sklearn
-os.chdir(r'C:\Users\최보경\Desktop\2018JUNIOR\01-데이터관리와지적경영\teamdata\final')
-
-
-# In[9]:
-
-
-#리스트 읽어오기
-may_df=pd.read_excel('MAY1(솔이님+트렌드+유동량변화율로정리).xls')
-station_list=list(may_df['station'].unique())
-
-
-# In[40]:
-
-
-x=pd.DataFrame()
-x=x.append(pd.Series(['d', 'e','f']),ignore_index=True )
-x=x.append(pd.Series(['d', 'e','f']),ignore_index=True )
-x
 
 
 # In[49]:
